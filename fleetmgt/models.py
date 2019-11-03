755b937ec39cc9306ab9255504246c3122fc0515
itumelengmaliehe <56831147+itumelengmaliehe@users.noreply.github.com>
2019-11-03 09:20:39 +0200

    Update models.py
    I added plate_no to the class vehicle and registration_no and deleted make cause i think model should cover the make of the car.

--- fleetmgt/models.py
+++ fleetmgt/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class fleetVehicle(models.Model):
+    plate_no = models.CharField (max_length=30)
+    registration_no = models.CharField (max_length=30)
     eng_no = models.CharField(max_length=30)
-    make = models.CharField(max_length=30)
     model = models.CharField(max_length=30)
     colour = models.CharField(max_length=30)
     year = models.CharField(max_length=30)
